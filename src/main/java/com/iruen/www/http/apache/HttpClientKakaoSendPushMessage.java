b1d41c094c08438f6e532e720dc47e245e8d9007
LeeDonghoon <skyfly33@users.noreply.github.com>
2015-07-21 11:30:59 +0900

    send message -2 pushMessageJson is not proper error

--- src/main/java/com/iruen/www/http/apache/HttpClientKakaoSendPushMessage.java
+++ src/main/java/com/iruen/www/http/apache/HttpClientKakaoSendPushMessage.java
@@ -3,6 +3,7 @@ package com.iruen.www.http.apache;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 import org.apache.http.HttpEntity;
@@ -12,20 +13,34 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.simple.JSONArray;
 
+import com.google.gson.GsonBuilder;
 import com.iruen.www.helper.Config;
+import com.iruen.www.kakao.domain.KakaoMessage;
 
 public class HttpClientKakaoSendPushMessage {
-	// HttpClient ª˝º∫
+	// HttpClient ÏÉùÏÑ±
 			private static HttpClient httpClient = HttpClientBuilder.create().build();
+			private static URIBuilder builder = new URIBuilder();
+			private static String uuid = Config.getInstance().getProperties("uuid");
 			private static String adminKey = "KakaoAK " + Config.getInstance().getProperties("adminKey");
+			private static HttpClientKakaoPushtokenRegister tokenRegister = new HttpClientKakaoPushtokenRegister();
 
+			@SuppressWarnings("unchecked")
 			public static void main(String[] args) {
+				tokenRegister.tokenRegister();
+				
+				builder.setScheme("https");
+				builder.setHost("kapi.kakao.com");
+				builder.setPath("/v1/push/send");
+				
 				try {
-					// HttpGetª˝º∫
-					HttpPost httpPost = new HttpPost("https://kapi.kakao.com/v1/push/send");
+					// HttpGetÏÉùÏÑ±
+					HttpPost httpPost = new HttpPost(builder.build());
 					
 					//Setup the request header
 					httpPost.addHeader("Authorization", adminKey);
@@ -34,10 +49,14 @@ public class HttpClientKakaoSendPushMessage {
 
 					// Setup the request parameters
 					ArrayList<NameValuePair> postParameters;
+					JSONArray uuids = new JSONArray();
+					uuids.add(uuid);
+					KakaoMessage message = KakaoMessage.getDummyMessage();
+					String message2JSonIsNull = new GsonBuilder().serializeNulls().create().toJson(message);					
 					
 					postParameters = new ArrayList<NameValuePair>(4);
-				    postParameters.add(new BasicNameValuePair("uuids", ""));
-				    postParameters.add(new BasicNameValuePair("push_message", ""));
+				    postParameters.add(new BasicNameValuePair("uuids", uuids.toJSONString()));
+				    postParameters.add(new BasicNameValuePair("push_message", message2JSonIsNull));
 				    
 				    httpPost.setEntity(new UrlEncodedFormEntity(postParameters));
 
@@ -45,7 +64,7 @@ public class HttpClientKakaoSendPushMessage {
 					HttpEntity entity = response.getEntity();
 
 					System.out.println("---------------------------------------- start ----------------------------------------");
-					// ¿¿¥‰ ∞·∞˙
+					// ÏùëÎãµ Í≤∞Í≥º
 					System.out.println(response.getStatusLine());
 					if (entity != null) {
 						System.out.println("Response content length: " + entity.getContentLength());
@@ -57,14 +76,16 @@ public class HttpClientKakaoSendPushMessage {
 						}
 					}
 					httpPost.abort();
-					System.out.println("---------------------------------------- end ----------------------------------------");
+					System.out.println("---------------------------------------- end ----------------------------------------\n");
 					httpClient.getConnectionManager().shutdown();
 
 				} catch (ClientProtocolException e) {
 					e.printStackTrace();
-				} catch (IOException ee) {
-					ee.printStackTrace();
-				} finally {
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch(URISyntaxException e){
+					e.printStackTrace();
+				}finally {
 					httpClient.getConnectionManager().shutdown();
 				}
 			}
