5acc7d7e0158b3f9b7c44236f2b00e39df4963fd
LeeDonghoon <skyfly33@users.noreply.github.com>
2015-07-21 14:40:06 +0900

    message build 수정.

--- src/main/java/com/iruen/www/http/apache/HttpClientKakaoSendPushMessage.java
+++ src/main/java/com/iruen/www/http/apache/HttpClientKakaoSendPushMessage.java
@@ -17,6 +17,8 @@ import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import com.google.gson.GsonBuilder;
 import com.iruen.www.helper.Config;
@@ -51,13 +53,18 @@ public class HttpClientKakaoSendPushMessage {
 					ArrayList<NameValuePair> postParameters;
 					JSONArray uuids = new JSONArray();
 					uuids.add(uuid);
-					KakaoMessage message = KakaoMessage.getDummyMessage();
-					String messageFromJson = new GsonBuilder().serializeNulls().create().toJson(message);					
+//					KakaoMessage message = KakaoMessage.getDummyMessage();
+					JSONObject customMessageObject = new JSONObject();
+					customMessageObject.put("message", "iruen dummy message!");
+					JSONObject messageObject = new JSONObject();
+					messageObject.put("custom_field", customMessageObject);
+					JSONObject gcmMessage = new JSONObject();
+					gcmMessage.put("for_gcm", messageObject);
 					
 					postParameters = new ArrayList<NameValuePair>(4);
 				    postParameters.add(new BasicNameValuePair("uuids", uuids.toJSONString()));
-				    System.out.println("message : " + messageFromJson);
-				    postParameters.add(new BasicNameValuePair("push_message", messageFromJson));
+				    System.out.println("message : " + messageObject.toJSONString());
+				    postParameters.add(new BasicNameValuePair("push_message", gcmMessage.toJSONString()));
 				    
 				    httpPost.setEntity(new UrlEncodedFormEntity(postParameters));
 
@@ -68,7 +75,8 @@ public class HttpClientKakaoSendPushMessage {
 					// 응답 결과
 					System.out.println(response.getStatusLine());
 					if (entity != null) {
-						System.out.println("Response content length: " + entity.getContentLength());
+						System.out.println("Response content length : " + entity.getContentLength());
+						System.out.println("Reponse Content-type : " + entity.getContentType());
 						BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 
 						String line = "";
