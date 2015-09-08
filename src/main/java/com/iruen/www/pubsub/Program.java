e8db4f2c82c91b6fa64649ab0a68edfeaf7cf04b
skyfly33 <imfly7@naver.com>
2015-09-08 10:28:52 +0900

    jedis version update.

--- src/main/java/com/iruen/www/pubsub/Program.java
+++ src/main/java/com/iruen/www/pubsub/Program.java
@@ -1,5 +1,7 @@
 package com.iruen.www.pubsub;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 
 import com.iruen.www.helper.JedisHelper;
@@ -7,33 +9,34 @@ import com.iruen.www.http.apache.HttpClientKakaoSendPushMessage;
 
 public class Program {
 
-	public static final String CHANNEL_NAME = "testchannel";
-	
+    private static final Logger logger = LoggerFactory.getLogger(Program.class);
 
-	public static void main(String[] args) throws Exception {
+    public static final String CHANNEL_NAME = "testchannel";
 
-		final JedisHelper helper = JedisHelper.getInstance();
-		final Jedis subscriberJedis = helper.getConnection();
-		final Subscriber subscriber = new Subscriber();
+    public static void main(String[] args) throws Exception {
 
-		new Thread(new Runnable() {
-			public void run() {
-				try {
-					System.out.println("Subscribing to \"testchannel\". This thread will be blocked.");
-					subscriberJedis.subscribe(subscriber, CHANNEL_NAME);
-					System.out.println("Subscription ended.");
-				} catch (Exception e) {
-					System.out.println("Subscribing failed." + e);
-				}
-			}
-		}).start();
+        final JedisHelper helper = JedisHelper.getInstance();
+        final Jedis subscriberJedis = helper.getConnection();
+        final Subscriber subscriber = new Subscriber();
 
-		final Jedis publisherJedis = helper.getConnection();
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    logger.info("Subscribing to \"testchannel\". This thread will be blocked.");
+                    subscriberJedis.subscribe(subscriber, CHANNEL_NAME);
+                    logger.info("Subscription ended.");
+                } catch (Exception e) {
+                    logger.info("Subscribing failed." + e);
+                }
+            }
+        }).start();
 
-		new Publisher(publisherJedis, CHANNEL_NAME).start();
+        final Jedis publisherJedis = helper.getConnection();
 
-		subscriber.unsubscribe();
-		helper.returnResource(subscriberJedis);
-		helper.returnResource(publisherJedis);
-	}
+        new Publisher(publisherJedis, CHANNEL_NAME).start();
+
+        subscriber.unsubscribe();
+        helper.returnResource(subscriberJedis);
+        helper.returnResource(publisherJedis);
+    }
 }
