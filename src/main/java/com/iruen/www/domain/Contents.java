c7abfdb778aac288f906d0151e30a106d97fd887
skyfly33 <imfly7@naver.com>
2015-09-08 11:20:25 +0900

    add contents model.

---
+++ src/main/java/com/iruen/www/domain/Contents.java
@@ -0,0 +1,17 @@
+package com.iruen.www.domain;
+
+/**
+ * Created by donghoon on 15. 9. 8..
+ */
+public class Contents {
+
+    private String en;
+
+    public String getEn() {
+        return en;
+    }
+
+    public void setEn(String en) {
+        this.en = en;
+    }
+}
