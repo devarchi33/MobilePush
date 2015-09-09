8056bb0e7a71c8e4c07367a6d30f015fbe727d6c
skyfly33 <imfly7@naver.com>
2015-09-09 09:50:54 +0900

    separate createsubscriber() method.

--- src/main/java/com/iruen/www/pubsub/Program.java
+++ src/main/java/com/iruen/www/pubsub/Program.java
@@ -11,25 +11,14 @@ public class Program {
 
     private static final Logger logger = LoggerFactory.getLogger(Program.class);
 
-    public static final String CHANNEL_NAME = "testchannel";
+    private static final String CHANNEL_NAME = "testchannel";
+    private static final JedisHelper helper = JedisHelper.getInstance();
+    private static final Jedis subscriberJedis = helper.getConnection();
+    private static final Subscriber subscriber = new Subscriber();
 
     public static void main(String[] args) throws Exception {
 
-        final JedisHelper helper = JedisHelper.getInstance();
-        final Jedis subscriberJedis = helper.getConnection();
-        final Subscriber subscriber = new Subscriber();
-
-        new Thread(new Runnable() {
-            public void run() {
-                try {
-                    logger.info("Subscribing to \"testchannel\". This thread will be blocked.");
-                    subscriberJedis.subscribe(subscriber, CHANNEL_NAME);
-                    logger.info("Subscription ended.");
-                } catch (Exception e) {
-                    logger.info("Subscribing failed." + e);
-                }
-            }
-        }).start();
+        createSubscriber(CHANNEL_NAME);
 
         final Jedis publisherJedis = helper.getConnection();
 
@@ -38,5 +27,19 @@ public class Program {
         subscriber.unsubscribe();
         helper.returnResource(subscriberJedis);
         helper.returnResource(publisherJedis);
+    }
+
+    public static void createSubscriber(String channelName) {
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    logger.info("Subscribing to \"" + channelName + "\". This thread will be blocked.");
+                    subscriberJedis.subscribe(subscriber, channelName);
+                    logger.info("Subscription ended.");
+                } catch (Exception e) {
+                    logger.info("Subscribing failed." + e);
+                }
+            }
+        }).start();
     }
 }
